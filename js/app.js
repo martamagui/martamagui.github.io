02c26e21a1524ee1c8664c3acf1de92784af92a7
Marta Molina <marta.molina@live.u-tad.com>
2021-09-09 13:55:16 +0200

    Wip

--- js/app.js
+++ js/app.js
@@ -16,17 +16,33 @@ const navbarElements = [
   "nav-contact",
   "nav-resume",
 ];
+const landingElements = [
+  "landing__p-1",
+  "myName",
+  "landing__h2-1",
+  "landing__p-2",
+  "landing__p-3",
+];
 const navList = document.getElementById("nav-list");
 console.log(navList);
+
+// fade in animation
 const addFadeInClass = (idName) => {
   let element = document.getElementById(idName);
-  element.classList.remove("d-none");
-  element.classList.add("fade-in");
+  element.classList.remove("d-occult");
+  element.classList.add("fade-in-down");
 };
 
 window.addEventListener("load", async () => {
+  let time = 0;
   for (let i = 0; i < navbarElements.length; ++i) {
-    let time = i * 350;
+    time = i * 200;
     setTimeout(() => addFadeInClass(navbarElements[i]), time);
+    console.log(time);
+  }
+  time = 1400;
+  for (let j = 0; j < landingElements.length; ++j) {
+    time += 450;
+    setTimeout(() => addFadeInClass(landingElements[j]), time);
   }
 });
