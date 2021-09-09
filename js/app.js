eb60e29f18a93866e5fe56d13597a029554f8b95
Marta Molina <marta.molina@live.u-tad.com>
2021-09-10 00:44:47 +0200

    Starting animating scroll down

--- js/app.js
+++ js/app.js
@@ -22,27 +22,65 @@ const landingElements = [
   "landing__h2-1",
   "landing__p-2",
   "landing__p-3",
+  "icon-linkedin",
+  "icon-github",
 ];
-const navList = document.getElementById("nav-list");
-console.log(navList);
 
-// fade in animation
-const addFadeInClass = (idName) => {
+// ----------------------- Load animation
+const addClass = (idName, className) => {
   let element = document.getElementById(idName);
   element.classList.remove("d-occult");
-  element.classList.add("fade-in-down");
+  element.classList.add(`${className}`);
 };
 
 window.addEventListener("load", async () => {
+  const screenScrollY =
+    document.documentElement.scrollHeight - window.innerHeight;
+  console.log(`Scroleable: ${screenScrollY}`);
+
   let time = 0;
   for (let i = 0; i < navbarElements.length; ++i) {
     time = i * 200;
-    setTimeout(() => addFadeInClass(navbarElements[i]), time);
+    setTimeout(() => addClass(navbarElements[i], "fade-in-down"), time);
     console.log(time);
   }
-  time = 1400;
   for (let j = 0; j < landingElements.length; ++j) {
-    time += 450;
-    setTimeout(() => addFadeInClass(landingElements[j]), time);
+    time += 280;
+    setTimeout(() => addClass(landingElements[j], "fade-in-up"), time);
   }
+});
+
+// ----------------------- Scroll fade in-up animation
+const aboutSection = [
+  "about__title",
+  "about__p-1",
+  "about__p-2",
+  "mosaic",
+  "about__p-3",
+];
+
+window.addEventListener("scroll", () => {
+  const screenScrollY =
+    document.documentElement.scrollHeight - window.innerHeight + 0.001;
+  //4866
+  const aboutScroll = screenScrollY * 0.0616;
+  let aboutBool = false;
+  const mySkillsScroll = screenScrollY * 0.205;
+  let mySkillsBool = false;
+  const experienceScroll = screenScrollY * 0.429;
+  let experienceBool = false;
+  const workScroll = screenScrollY * 0.596;
+  let workBool = false;
+  const contactScroll = screenScrollY * 0.883;
+  let contactBool = false;
+  const currentScroll = window.scrollY;
+  let time = 10;
+  if (currentScroll >= aboutScroll && aboutBool === false) {
+    for (let i = 0; i < aboutSection.length; ++i) {
+      console.log("entra");
+      setTimeout(() => addClass(aboutSection[i], "fade-in-up"), time);
+      time += Math.random(200) + time;
+    }
+  }
+  console.log(`Current: ${currentScroll}`);
 });
